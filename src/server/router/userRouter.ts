6cf60fac78c55cea1d6e2009b85022ff4f667756
happi89 <farhaanp99@gmail.com>
2022-09-15 08:11:03 -0400

    added bio in schema and added update user in router

--- src/server/router/userRouter.ts
+++ src/server/router/userRouter.ts
@@ -2,28 +2,52 @@ import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { createRouter } from './context';
 
-export const userRouter = createRouter().query('getOne', {
-	input: z.object({
-		name: z.string(),
-	}),
-	async resolve({ ctx, input }) {
-		try {
-			return await ctx.prisma.user.findUnique({
-				where: {
-					name: input.name,
-				},
-				include: {
-					posts: true,
-					comments: true,
-					subRedditsJoined: true,
-					_count: {
-						select: { comments: true, posts: true, subRedditsJoined: true },
+export const userRouter = createRouter()
+	.query('getOne', {
+		input: z.object({
+			name: z.string(),
+		}),
+		async resolve({ ctx, input }) {
+			try {
+				return await ctx.prisma.user.findUnique({
+					where: {
+						name: input.name,
 					},
+					include: {
+						posts: true,
+						comments: true,
+						subRedditsJoined: true,
+						_count: {
+							select: { comments: true, posts: true, subRedditsJoined: true },
+						},
+					},
+				});
+			} catch (err) {
+				console.log('error', err);
+				throw new TRPCError({ code: 'BAD_REQUEST' });
+			}
+		},
+	})
+	.middleware(async ({ ctx, next }) => {
+		if (!ctx.session) {
+			throw new TRPCError({ code: 'UNAUTHORIZED' });
+		}
+		return next();
+	})
+	.mutation('updateUser', {
+		input: z.object({
+			name: z.string().optional(),
+			bio: z.string().optional(),
+		}),
+		async resolve({ ctx, input }) {
+			return await ctx.prisma.user.update({
+				where: {
+					id: ctx.session?.user?.id,
+				},
+				data: {
+					name: input?.name,
+					bio: input?.bio,
 				},
 			});
-		} catch (err) {
-			console.log('error', err);
-			throw new TRPCError({ code: 'BAD_REQUEST' });
-		}
-	},
-});
+		},
+	});
